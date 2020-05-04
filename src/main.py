8a7f59a5f6dd32cc16b5e22f15e6350dc29e7d9d
kadepeay <kadepeay@gmail.com>
2020-05-04 12:44:34 -0600

    Board initialized

---
+++ src/main.py
@@ -0,0 +1,13 @@
+#!/bin/env python3
+
+def boardinit():
+    board = [['-', '-', '-'],
+    ['-', '-', '-'],
+    ['-', '-', '-']]
+
+    for row in board:
+        print(row)
+
+
+if __name__ == "__main__":
+    boardinit()
